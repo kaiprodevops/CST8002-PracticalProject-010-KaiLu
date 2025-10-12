0157f4c73e7b618de364f62b18448b5e998d849a
kaiprodev <kaiprodevops@gmail.com>
2025-10-12 01:27:06 -0400

    create a nunit to start

---
+++ PracticalProject2.NUnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace PracticalProject2.NUnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
