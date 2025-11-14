63c6fc61d4817840dd02a721d0e954e13b17e2ef
kaiprodev <kaiprodevops@gmail.com>
2025-11-14 10:42:57 -0500

    clean up

--- PracticalProject2/Program.cs
+++ PracticalProject2/Program.cs
@@ -1,6 +1,6 @@
 ﻿/// <summary>
 /// Course number: CST8002
-/// Course name: Programming Language Research Project
+/// Course name: Programming Language Research Project.
 /// Professor's name: Stanley Pieda
 /// Due date: 2025-11-16
 /// Author name: Kai Lu
